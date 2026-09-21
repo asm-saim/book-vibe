db9deca40d5defc1d875dab67f54078064401be7
asm-saim <saim777m@gmail.com>
2026-09-21 11:44:28 +0600

    navbar initiated

---
+++ src/app/page-layout/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>page-layout</div>;
+};
+
+export default page;
---
+++ src/components/shared/Navabr.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import React from "react";
+import logo from "@/assets/book.ico";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              aria-label="Menu"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />{" "}
+            </svg>
+          </div>
+          <ul tabIndex={-1} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex gap-2 items-center">
+          <Image src={logo} width={40} height={40}></Image>
+          <p className="font-bold text-xl">Book Vibe</p>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <a>Item 3</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end gap-2">
+        <button className="btn bg-green-700">Sign In</button>
+        <button className="btn bg-blue-400">Sign Up</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
